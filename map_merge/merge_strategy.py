e0b3919a6afc08e500989dc13fbcd97f263210af
Guy Grinwald <guy@grinwald.com>
2022-12-03 17:09:22 +0200

    Added 2 numeric merging strategies

--- map_merge/merge_strategy.py
+++ map_merge/merge_strategy.py
@@ -1,10 +1,54 @@
 from abc import ABC, abstractmethod
+from numbers import Number
+from typing import Iterable
 
 class MergeStrategy(ABC):
+    """
+    An abstract calss that sets the interface for classes able to merge 2 elements in some way
+    """
+    def __init__(self, none=None):
+        self.NONE = none
 
     @abstractmethod
     def merge(self, value1, value2):
+        """
+        Merges the given values
+        """
         pass
 
+
+class NumericSum(MergeStrategy):
+    """
+    Merges elements by performing the arithmatic addition operation
+    """
+    def __init__(self):
+        super().__init__(0)
+
+    def merge(self, value1: int, value2: int) -> int:
+        """
+        Merges the given values by arithmatic addition
+        """
+
+        if not isinstance(value1, Number) or not isinstance(value2, Number):
+            raise ValueError("Given arguments are not numbers")
+
+        return value1 + value2
+
+
+class NumericMultiplication(MergeStrategy):
+    """
+    Merges elements by performing the arithmatic multiplication operation
+    """
+    def __init__(self):
+        super().__init__(0)
+
+    def merge(self, value1: int, value2: int) -> int:
+        """
+        Merges the given values by arithmatic multiplication
+        """
+        if not isinstance(value1, Number) or not isinstance(value2, Number):
+            raise ValueError("Given arguments are not numbers")
+
+        return value1 * value2
 
 
